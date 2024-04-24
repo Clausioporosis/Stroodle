fafbb7fca02f84306a612992bec2e81e8716dd40
clscit00 <clscit00@hs-esslingen.de>
2024-04-24 11:03:26 +0200

    Started to make thew WeekView component Wrong branch, but more important to get that mostly working now

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Dashboard.css';
 import Header from "../common/header/Header"
+import WeekView from "../shared/WeekView"
 
 const Dashboard: React.FC = () => {
     return (
         <div className="dashboard-container">
             <Header />
+            <WeekView width='600px' visibleRows={6} />
         </div>
     );
 };
