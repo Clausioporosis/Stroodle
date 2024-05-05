5b0b9c0068bccae37ad53f51f33d33b56b08091f
clscit <147387660+clscit@users.noreply.github.com>
2024-05-06 00:39:26 +0200

    working on form for poll creation

--- frontend/src/components/polls/createPoll/CreatePoll.tsx
+++ frontend/src/components/polls/createPoll/CreatePoll.tsx
@@ -5,6 +5,14 @@ import PollService from '../../../services/PollService';
 import { Poll } from '../../../models/Poll';
 
 const Dashboard: React.FC = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [location, setLocation] = useState('');
+    const [participants, setParticipants] = useState('');
+    const [duration, setDuration] = useState('');
+    const [timeLimit, setTimeLimit] = useState(false);
+    const [participantLimit, setParticipantLimit] = useState(false);
+    const [reminder, setReminder] = useState(false);
 
     useEffect(() => {
 
@@ -15,7 +23,43 @@ const Dashboard: React.FC = () => {
         <div className="create-poll-container">
             <Header />
             <div className="create-poll-content-container">
+                <div className="left-section-container">
+                    <form>
+                        <h1>Umfrage erstellen</h1>
+                        <h3>Titel </h3>
+                        <input type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="Was ist der Anlass?" />
+                        <h3>Beschreibung</h3>
+                        <textarea value={description} onChange={e => setDescription(e.target.value)} placeholder="Was muss man wissen?" />
+                        <h3>Ort</h3>
+                        <input type="text" value={location} onChange={e => setLocation(e.target.value)} placeholder="Wo wird es statt finden?" />
+                        <h3>Teilnehmer</h3>
+                        <div className="more-settings-container">
+                            <h3 className="more-settings-header">Weitere Einstellungen</h3>
+                            <div className="header-border"></div>
+                        </div>
+                        {/*
+                        <input type="text" value={participants} onChange={e => setParticipants(e.target.value)} placeholder="Participants" />
+                        Additional participant search and add functionality
+                        <input type="text" value={duration} onChange={e => setDuration(e.target.value)} placeholder="Duration" />
+                        <label>
+                            <input type="checkbox" checked={timeLimit} onChange={e => setTimeLimit(e.target.checked)} />
+                            Time Limit
+                        </label>
+                        <label>
+                            <input type="checkbox" checked={participantLimit} onChange={e => setParticipantLimit(e.target.checked)} />
+                            Participant Limit
+                        </label>
+                        <label>
+                            <input type="checkbox" checked={reminder} onChange={e => setReminder(e.target.checked)} />
+                            Reminder
+                        </label>
+                        */}
+                    </form>
+                </div>
+                <div className="right-section-container">
 
+
+                </div>
             </div>
         </div>
     );
