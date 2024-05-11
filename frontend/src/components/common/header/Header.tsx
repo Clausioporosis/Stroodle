a46b15a2d0e9b6f10d32ab435ac1fc0726c60055
clscit <147387660+clscit@users.noreply.github.com>
2024-05-11 17:28:47 +0200

    Header dropdown works now as intended

--- frontend/src/components/common/header/Header.tsx
+++ frontend/src/components/common/header/Header.tsx
@@ -6,34 +6,32 @@ const Header: React.FC = () => {
     const toggleDropdown = () => setIsOpen(!isOpen);
 
     return (
-        <header className='app-header'>
-
-            <div className='start'>
-                <img src={process.env.PUBLIC_URL + '/stroodle-logo-white.png'} alt='Logo' className='stroodle-logo' />
+        <div className='app-header'>
+            {/* nasty solution, but now the dropdown gets rendered behind the header while still being useable */}
+            <div className={`dropdown-container ${isOpen ? 'visible' : ''}`}>
+                <a href='/dashboard'>Dashboard</a>
+                <a href='/availability'>Verfügbarkeit</a>
+                <a href='/profile'>Profil</a>
+                <a href='/login'>Abmelden</a>
             </div>
 
-            <div className='end'>
-                <div className='button-container'>
-                    <button className='profile-button'>
-                        <PersonCircle className='icon profile-icon' />
-                    </button>
-                    <button className='list-button' onClick={toggleDropdown}>
-                        <List className='icon list-icon' />
-                    </button>
+            <div className='app-header'>
+                <div className='start'>
+                    <img src={process.env.PUBLIC_URL + '/stroodle-logo-white.png'} alt='Logo' className='stroodle-logo' />
                 </div>
-                {isOpen && (
-                    <div className='dropdown-container'>
-                        <a href='/dashboard'>Dashboard</a>
-                        <a href='/availability'>Verfügbarkeit</a>
-                        <a href='/profile'>Profil</a>
-                        <a href='/login'>Abmelden</a>
+
+                <div className='end'>
+                    <div className='button-container'>
+                        <button className='profile-button'>
+                            <PersonCircle className='icon profile-icon' />
+                        </button>
+                        <button className='list-button' onClick={toggleDropdown}>
+                            <List className='icon list-icon' />
+                        </button>
                     </div>
-                )}
-
+                </div>
             </div>
-
-
-        </header>
+        </div>
     );
 };
 
