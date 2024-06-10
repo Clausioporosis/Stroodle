f0a36792ae6b691513a0e6e14f4a99bf8168e1eb
clscit00 <ovidiu.schmidtclaudio@gmail.com>
2024-06-10 23:48:52 +0200

    removed some unused dependencies and packages

--- backend/src/main/java/com/stroodle/backend/config/KeycloakConfig.java
+++ backend/src/main/java/com/stroodle/backend/config/KeycloakConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,12 +13,21 @@ public class KeycloakConfig {
     @Autowired
     private HttpServletRequest request;
 
+    @Value("${keycloak.auth-server-url:}")
+    private String authServerUrl;
+
+    @Value("${keycloak.realm:}")
+    private String realm;
+
+    @Value("${keycloak.resource:}")
+    private String resource;
+
     public Keycloak keycloak() {
         String token = request.getHeader("Authorization").substring(7);
         return KeycloakBuilder.builder()
-                .serverUrl("https://login.stroodle.online")
-                .realm("Stroodle")
-                .clientId("stroodle-rest-api")
+                .serverUrl(authServerUrl)
+                .realm(realm)
+                .clientId(resource)
                 .authorization(token)
                 .build();
     }
